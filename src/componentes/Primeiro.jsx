77b568cc1225488f71cfd762c97caefd09f2dc4a
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-01-13 12:09:05 -0300

    Criar funcao com arrow function.

---
+++ src/componentes/Primeiro.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => 
+        //elementos adjacenter nao serao exibidos a menos que seja colocado entre tags vazias ou atraves do React.Fragment
+        <React.Fragment>
+            <h1>Primeiro Componente (Arrow)</h1>
+            <h2>Exemplo de um primeiro componente</h2>
+
+        </React.Fragment>
